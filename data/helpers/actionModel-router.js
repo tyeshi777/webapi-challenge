a02b29b5001468d30c2ed5bd963fa8028336df23
Yeshi <tyeshi777@yahoo.com>
2019-04-26 09:01:47 -0700

    actions completed

--- data/helpers/actionModel-router.js
+++ data/helpers/actionModel-router.js
@@ -18,10 +18,49 @@ router.get("/:id", (req, res) => {
   const actionId = req.params.id;
   action
     .get(actionId)
-    .then()
+    .then(action => {
+      res.status(200).json(action);
+    })
     .catch(err => {
       res.status(500).json({ err: "no such id" });
     });
 });
 
+router.post("/", (req, res) => {
+  const newAction = req.body;
+  action
+    .insert(newAction)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "cannot insert" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const actionId = req.params.id;
+  const updatedAction = req.body;
+  action
+    .update(actionId, updatedAction)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "cannot update" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const actionId = req.params.id;
+
+  action
+    .remove(actionId)
+    .then(action => {
+      res.status(204).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "cannot remove" });
+    });
+});
 module.exports = router;
